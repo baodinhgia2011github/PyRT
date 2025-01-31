d1a31fde9807d3478c9268cc8832a7b029948759
baodinhgia2011github <166897977+baodinhgia2011github@users.noreply.github.com>
2025-01-31 16:18:47 +0700

    Update RTC.py
    Fix battery issues and add calendar for current month

--- RTC.py
+++ RTC.py
@@ -3,13 +3,19 @@ import platform
 import time
 import ctypes
 import datetime
+import calendar
 from time import sleep
-if not platform.system()=="Windows":
-    import psutil
-FOREGROUND_GREEN=0x0A
-FOREGROUND_RED=0x0C
-FOREGROUND_YELLOW=0x0E
-FOREGROUND_WHITE=0x07
+if platform.system() is not "Windows":
+    try:
+        import psutil
+    except ImportError:
+        print("ERROR: psutil library is not detected.\nPlease check your Python version or install 'psutil' library using 'pip install psutil' and try again.")
+        exit()
+if platform.system() is "Windows":
+    FOREGROUND_GREEN=0x0A
+    FOREGROUND_RED=0x0C
+    FOREGROUND_YELLOW=0x0E
+    FOREGROUND_WHITE=0x07
 class SystemPowerStatus(ctypes.Structure):
     _fields_=[
         ("ACLineStatus",ctypes.c_byte),
@@ -21,9 +27,7 @@ class SystemPowerStatus(ctypes.Structure):
     ]
 def setConsoleColor(color):
     if platform.system()=="Windows":
-        ctypes.windll.kernel32.SetConsoleTextAttribute(
-            ctypes.windll.kernel32.GetStdHandle(-11),color
-        )
+        ctypes.windll.kernel32.SetConsoleTextAttribute(ctypes.windll.kernel32.GetStdHandle(-11),color)
     else:
         colors={
             FOREGROUND_GREEN:"\033[32m",
@@ -36,77 +40,91 @@ def clearConsole():
     os.system("CLS" if platform.system()=="Windows" else "clear")
 def displayTimeAndDate():
     currentTime=datetime.datetime.now()
-    DayOfWeek=currentTime.strftime("%A")
-    MonthOfYear=currentTime.strftime("%B")
+    dayOfWeek=currentTime.strftime("%A")
+    monthOfYear=currentTime.strftime("%B")
     date=currentTime.day
     year=currentTime.year
     hour=currentTime.hour
     minute=currentTime.minute
     second=currentTime.second
-    suffix=["th","st","nd","rd"]+["th"]*16+["st","nd","rd"]+["th"]*7
+    suffix=[None]+["th"]*31
+    for i in [1,21,31]:
+        suffix[i]="st"
+    for i in [2,22]:
+        suffix[i]="nd"
+    for i in [3,23]:
+        suffix[i]="rd"
     setConsoleColor(FOREGROUND_RED)
     print(f"Current time: {hour:02}:{minute:02}:{second:02}")
     setConsoleColor(FOREGROUND_GREEN)
-    print(f"Current date: {DayOfWeek}, {MonthOfYear} {date:02}{suffix[date%10]},{year}")
+    print(f"Current date: {dayOfWeek}, {monthOfYear} {date}{suffix[date]}, {year}")
     setConsoleColor(FOREGROUND_WHITE)
     print("\n")
+def displayCurrentMonthCalendar():
+    currentTime=datetime.datetime.now()
+    monthOfYear=currentTime.strftime("%B")
+    month=currentTime.month
+    year=currentTime.year
+    print(calendar.month(year,month))
 def displayBatteryStatus():
-    print("\033[1mBattery Status:\033[0m")
-    if platform.system()=="Windows":
+    print("\033[1mBattery status:\033[0m")
+    if platform.system() is "Windows":
         powerStatus=SystemPowerStatus()
-        result=ctypes.windll.kernel32.GetSystemPowerStatus(
-            ctypes.pointer(powerStatus)
-            )
-        if result:
-            ACLine="Connected" if powerStatus.ACLineStatus else "Not connected"
-            print(f"AC line: ",end="")
-            setConsoleColor(FOREGROUND_GREEN if ACLine=="Connected" else FOREGROUND_RED)
-            print(f"{ACLine}")
-            batteryPercentage=powerStatus.BatteryLifePercent
-            if batteryPercentage>=40:
-                setConsoleColor(FOREGROUND_GREEN)
-            elif batteryPercentage>15:
-                setConsoleColor(FOREGROUND_YELLOW)
-            else:
-                setConsoleColor(FOREGROUND_RED)
-            print(f"Battery percentage: {batteryPercentage}%")
-            setConsoleColor(FOREGROUND_WHITE)
-            batteryLifeTime=powerStatus.BatteryLifeTime
-            if batteryLifeTime!=0xFFFFFFFF:
-                hours=batteryLifeTime//3600
-                minutes=(batteryLifeTime%3600)//60
-                print(f"Remaining battery life: {hours} hour(s) {minutes} minute(s)")
-            else:
-                print("Remaining battery life: ",end="")
-                setConsoleColor(FOREGROUND_YELLOW)
-                print("Unknown (Unable to calculate or AC connected)")
-        else:
+        if not ctypes.windll.kernel32.GetSystemPowerStatus(ctypes.pointer(powerStatus)):
             setConsoleColor(FOREGROUND_RED)
             print("ERROR: Failed to retrieve power status.")
-    elif platform.system() in ["Linux","Darwin"]:
+            setConsoleColor(FOREGROUND_WHITE)
+            return
+        ACLine=("Connected" if powerStatus.ACLineStatus else 
+                "Not connected" if not powerStatus.ACLineStatus else 
+                "Unknown"
+        )
+        print("AC Line: ",end="")
+        setConsoleColor(FOREGROUND_GREEN if ACLine=="Connected" else 
+                        FOREGROUND_YELLOW if ACLine=="Not connected" else 
+                        FOREGROUND_RED
+        )
+        print(f"{ACLine}")
+        batteryPercentage=powerStatus.BatteryLifePercent
+        setConsoleColor(FOREGROUND_GREEN if batteryPercentage>=40 else 
+                        FOREGROUND_YELLOW if batteryPercentage>15 else 
+                        FOREGROUND_RED
+        )
+        print(f"Battery Percentage: {batteryPercentage}%")
+        setConsoleColor(FOREGROUND_WHITE)
+        if not powerStatus.BatteryLifeTime==0xFFFFFFFF:
+            hours,minutes=divmod(powerStatus.BatteryLifeTime//60,60)
+        else:
+            print("Remaining Battery Life: ",end='')
+            setConsoleColor(FOREGROUND_YELLOW)
+            print("Unknown (Unable to connected or AC connected).")
+    elif platform.system() is "Linux" or "Darwin":
         battery=psutil.sensors_battery()
         if battery:
-            ACLine="Connected" if battery.power_plugged else "Not connected"
-            print(f"AC line: ",end="")
-            setConsoleColor(FOREGROUND_GREEN if battery.power_plugged else FOREGROUND_RED)
-            print(f"{ACLine}")
-            batteryPercentage=battery.percent
-            if batteryPercentage>=40:
-                setConsoleColor(FOREGROUND_GREEN)
-            elif batteryPercentage>15:
-                setConsoleColor(FOREGROUND_YELLOW)
-            else:
-                setConsoleColor(FOREGROUND_RED)
-            print(f"Battery percentage: {batteryPercentage}%")
+            print("AC Line: ",end="")
+            setConsoleColor(FOREGROUND_GREEN if battery.power_plugged else
+                            FOREGROUND_RED
+            )
+            print("Connected" if battery.power_plugged else
+                  "Not connected"
+            )
+            setConsoleColor(
+                FOREGROUND_GREEN if battery.percent>=40 else
+                FOREGROUND_YELLOW if battery.percent>15 else
+                FOREGROUND_RED
+            )
+            print(f"Battery Precentage: {battery.percent}%")
             setConsoleColor(FOREGROUND_WHITE)
-            if not battery.power_plugged:
-                hours,minutes=divmod(battery.secsleft//60,60)
-                print(f"Remaining battery life: {hours} hour(s) {minutes} minute(s)")
+            if battery.secsleft==psutil.POWER_TIME_UNLIMITED:
+                print("Remaining Battery Life: Unlimited (AC connected)")
             else:
-                print("Battery is charging or fully charged.")
+                hours,minutes=divmod(battery.secsleft//60,60)
+                print(f"Remaining Battery Life: {hours} hour(s) {minutes} minute(s)")
         else:
+            setConsoleColor(FOREGROUND_RED)
             print("Battery information not available.")
     else:
+        setConsoleColor(FOREGROUND_RED)
         print("Battery status not supported on this OS.")
     setConsoleColor(FOREGROUND_WHITE)
     print("\n")
@@ -119,18 +137,17 @@ def displayDeviceInformation():
     print("\n")
 def warning():
     if platform.system() not in ["Windows","Linux","Darwin"]:
-        print("This program is optimized for Windows, Linux, and MacOS.","Some features may not work",sep="\n")
-    print("Thank you for using RTC")
+        print("This program is optimized for Windows, Linux and MacOS.","Some featues may not work",sep="\n")
+    print("Thank you for using RTC.")
     sleep(2)
 def recommendation():
     clearConsole()
-    print("Recommended using this program with Visual Studio or")
-    print("Visual Studio Code with Python 3.13 or higher version that has been installed")
+    print("Recommended using this program with Visual Studio or\nVisual Studio Code with Python 3.13 or higher version that has been installed.")
     sleep(1)
 def preparation():
     print("Starting project",end="")
     for _ in range(3):
-        print('.',end='')
+        print('.',end="")
         sleep(1)
     clearConsole()
 def main():
@@ -146,7 +163,8 @@ def main():
             displayTimeAndDate()
             displayBatteryStatus()
             displayDeviceInformation()
-            print("\nTo exit program, please press Ctrl+C to terminate")
+            displayCurrentMonthCalendar()
+            print("\nTo exit program, press Ctrl+C to terminate")
         previousSecond=second
         sleep(0.5)
 if __name__=="__main__":
